5b544628b5d030554a3a31306bde062331be2c17
Emmanuel Geoffray <manuel@soixantecircuits.fr>
2013-11-19 16:22:07 +0100

    POO python

--- pyying.py
+++ pyying.py
@@ -9,90 +9,123 @@ import time
 from OSC import *   #required, to install : sudo pip install pyOSC
 
 
-def quit_pressed():
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            return True
-        if event.type == pygame.KEYDOWN :
-          if event.key == pygame.K_SPACE :
-            global isShooting
-            isShooting = True
-    return False
 
-def show(file):
-    picture = pygame.image.load(file)
-    main_surface.blit(picture, (0, 0))
-    pygame.display.flip()
+class Pyying:
+    snap_path = 'snaps/'
+    snap_filename = 'snap'
+    path = '/tmp/stream/'
+    filename = 'preview'
+    extension = 'jpg'
+    number = 0
+    snap_number = 0
+    
+    oscServer = None
+    oscThread = None
+    main_surface = None
 
-def printing_handler(addr, tags, stuff, source):
-    msg_string = "%s [%s] %s" % (addr, tags, str(stuff))
-    sys.stdout.write("OSCServer Got: '%s' from %s\n" % (msg_string, getUrlStr(source)))
-    return
+    isStreaming = True
+    isShooting = False
 
-def stream_handler(addr, tags, data, client):
-    print "Stream: " + str(data) + " is that " + str(True) + " ?"
-    global isStreaming
-    isStreaming = data
-    return
+    def __init__(self, host="0.0.0.0", port=6666):
+        
+        # osc
+        self.oscServer = OSCServer((host, int(port)))
+        self.oscServer.addDefaultHandlers()
+        self.oscServer.addMsgHandler("/pyying/stream", self.stream_handler)
+        self.oscServer.addMsgHandler("/pyying/shoot", self.shoot_handler)
+        self.oscThread = threading.Thread(target=self.oscServer.serve_forever)
+        self.oscThread.start()
+        print "Starting OSCServer. Use ctrl-C to quit."
 
-def shoot_handler(addr, tags, data, client):
-    print 'Shoot!'
-    global isShooting
-    isShooting = True
-    return
+        try:
+          # check folders exist
+          if not os.path.exists(self.path):
+                os.makedirs(self.path)
+          if not os.path.exists(self.snap_path):
+                os.makedirs(self.snap_path)
 
-s = OSCServer(('localhost', 6666))
-s.addDefaultHandlers()
-s.addMsgHandler("default", printing_handler)
-s.addMsgHandler("/pyying/stream", stream_handler)
-s.addMsgHandler("/pyying/shoot", shoot_handler)
-st = threading.Thread(target=s.serve_forever)
-st.start()
-print "Starting OSCServer. Use ctrl-C to quit."
+          # camera
+          self.camera = piggyphoto.camera()
+          self.camera.leave_locked()
+          fullpath = self.path + self.filename + str(self.number) + '.' + self.extension
+          self.camera.capture_preview(fullpath)
 
-path = '/tmp/stream/'
-filename = 'preview'
-extension = 'jpg'
-number = 0
-snap_path = 'snaps/'
-snap_number = 0
-fullpath = path + filename + str(number) + '.' + extension
-if not os.path.exists(path):
-      os.makedirs(path)
+          # create window from first preview 
+          picture = pygame.image.load(fullpath)
+          pygame.display.set_mode(picture.get_size())
+          self.main_surface = pygame.display.get_surface()
+        except KeyboardInterrupt:
+          self.close()
+        except Exception as e:
+          print str(e) 
+          self.close()
 
-clock = pygame.time.Clock()
-try:
-  C = piggyphoto.camera()
-  C.leave_locked()
-  C.capture_preview(fullpath)
+    def start(self):
+        clock = pygame.time.Clock()
+        try:
+          while not self.quit_pressed():
+            # trying to get a fixed fps. However the camera is limiting to approx 22 fps
+            print str(clock.get_fps())
+            clock.tick(25)
+            
+            # Shoot picture
+            if (self.isShooting):
+              self.isShooting = False
+              print 'Shoot!'
+              fullpath = self.snap_path + self.snap_filename + str(self.snap_number) + self.extension
+              self.camera.capture_image(fullpath)
+              self.snap_number+=1
 
-  picture = pygame.image.load(fullpath)
-  pygame.display.set_mode(picture.get_size())
-  main_surface = pygame.display.get_surface()
+            # Stream pictures
+            if (self.isStreaming):
+                fullpath = self.path + self.filename + str(self.number) + '.' + self.extension
+                self.camera.capture_preview(fullpath)
+                self.show(fullpath)
+                self.number += 1
+          self.close()
 
-  isStreaming = True
-  isShooting = False
+        except KeyboardInterrupt:
+          self.close()
+        except Exception as e:
+          print str(e) 
+          self.close()
 
-  oldtime = time.time()
-  while not quit_pressed():
-    print str(clock.get_fps())
-    clock.tick(25)
-    if (isShooting):
-      isShooting = False
-      print "Shoot"
-      C.capture_image(snap_path + 'snap' + str(snap_number) + '.jpg')
-      snap_number+=1
-    if (isStreaming):
-        fullpath = path + filename + str(number) + '.' + extension
-        C.capture_preview(fullpath)
-        show(fullpath)
-        number += 1
-except KeyboardInterrupt:
-  pass
-except Exception as e:
-  print str(e) 
-  pass
 
-# close    
-s.close()
-st.join()
+    def close(self):
+        self.oscServer.close()
+        self.oscThread.join()
+
+    def quit_pressed(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return True
+            if event.type == pygame.KEYDOWN :
+              if event.key == pygame.K_SPACE :
+                self.isShooting = True
+        return False
+
+    def show(self,file):
+        picture = pygame.image.load(file)
+        self.main_surface.blit(picture, (0, 0))
+        pygame.display.flip()
+
+    def stream_handler(self, addr, tags, data, client):
+        print "Stream: " + str(data) + " is that " + str(True) + " ?"
+        self.isStreaming = data
+        return
+
+    def shoot_handler(self, addr, tags, data, client):
+        self.isShooting = True
+        return
+
+
+
+
+
+def main(script, host="0.0.0.0", port=6666):
+    ying = Pyying(host, port)
+    ying.start()
+
+
+if __name__ == '__main__':
+	main(*sys.argv)
