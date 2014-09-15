2e07be8f60e8f31ad25b209828e0312b9289b9a0
hugohil <hugo@soixantecircuits.fr>
2014-09-15 16:21:31 +0200

    change pyOSC installation mode

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
-sudo apt-get install python-pygame python-pip gphoto2 libgphoto2-2-dev libgphoto2-port0
-sudo pip install pyOSC
-
-cd ~/sources
-mkdir python
-mkdir python/soixante
-cd python/soixante
-git clone git@github.com:soixantecircuits/pyying.git
-cd ~/sources
 sudo apt-get install libjpeg62-dev imagemagick subversion
+sudo apt-get install python-pygame python-pip gphoto2 libgphoto2-2-dev libgphoto2-port0
+# sudo pip install --allow-unverified pyOSC
+
+cd ../
+git clone https://github.com/ptone/pyosc
+cd pyosc
+sudo ./setup.py install
+cd ../pyying
+
 svn co https://svn.code.sf.net/p/mjpg-streamer/code/mjpg-streamer
 cd mjpg-streamer
 make
