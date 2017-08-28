9ebf0909cd091cf9c4670a44ab4f2418152e07d6
Emmanuel Geoffray <emmanuel@soixantecircuits.fr>
2017-08-28 17:14:31 +0000

    add api route to set camera config

--- tests/test.py
+++ tests/test.py
@@ -12,7 +12,7 @@ from pySpacebroClient import SpacebroClient
 import pyStandardSettings
 from dotmap import DotMap
 
-timeout=1
+timeout=2
 shoot_timeout=6
 verbose=True
 
@@ -20,6 +20,46 @@ class TestSpacebroClient(unittest.TestCase):
 
     def on_inMedia(self, args):
         self.inMediaValue = args
+
+    def on_inConfig(self, args):
+        self.config = args
+
+    def test_config(self):
+        settings = DotMap(pyStandardSettings.getSettings())
+        spacebroSettings = settings.service.spacebro
+        spacebroSettings.clientName = "pyying-test"
+        spacebroClient = SpacebroClient(spacebroSettings.toDict())
+
+        # Listen
+        spacebroClient.on(str(spacebroSettings.client['out'].config.eventName), self.on_inConfig)
+        spacebroClient.wait(seconds=timeout)
+        spacebroClient.emit(str(spacebroSettings.client['in'].getConfig.eventName), {})
+        spacebroClient.wait(seconds=timeout)
+
+        print self.config
+        # test good format
+        self.assertEqual(self.config['main']['capturesettings']['aperture']['label'], 'Aperture')
+
+        # test change value
+        self.config['main']['capturesettings']['aperture']['value'] = '7.1'
+        spacebroClient.emit(str(spacebroSettings.client['in'].setConfig.eventName), self.config)
+        spacebroClient.wait(seconds=timeout)
+        self.config = {}
+        spacebroClient.emit(str(spacebroSettings.client['in'].getConfig.eventName), {})
+        spacebroClient.wait(seconds=timeout)
+        self.assertEqual(self.config['main']['capturesettings']['aperture']['value'], '7.1')
+
+        # test change value again
+        self.config['main']['capturesettings']['aperture']['value'] = '6.3'
+        spacebroClient.emit(str(spacebroSettings.client['in'].setConfig.eventName), self.config)
+        spacebroClient.wait(seconds=timeout)
+        self.config = {}
+        spacebroClient.emit(str(spacebroSettings.client['in'].getConfig.eventName), {})
+        spacebroClient.wait(seconds=timeout)
+        self.assertEqual(self.config['main']['capturesettings']['aperture']['value'], '6.3')
+
+        spacebroClient.disconnect()
+
 
     def test_shoot(self):
         settings = DotMap(pyStandardSettings.getSettings())
